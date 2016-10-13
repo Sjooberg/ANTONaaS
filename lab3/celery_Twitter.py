11b455ad824938ef19ca7568e39581965ce5953a
Sjooberg <anton.sjoberg.1992@gmail.com>
2016-10-14 00:10:05 +0200

    Update celery_Twitter.py

--- lab3/celery_Twitter.py
+++ lab3/celery_Twitter.py
@@ -1,9 +1,12 @@
+from __future__ import division
 from flask import Flask, render_template
 from tasks import make_celery
 import os
 import sys
 import swiftclient
 from collections import Counter
+import pygal
+import ast
 try:
 	import json
 except ImportError:
@@ -40,11 +43,34 @@ def twitterCount():
 	tweetRetrieveAndCount.delay()
 	return "End flask route"
 
+#Create the Graph
+@app.route('/graph', methods=["GET"])
+def graph():#
+	if os.path.isfile("result"):
+		pronoun_graph = pygal.Bar(width=1200, height=600,
+                          explicit_size=True)
+		with open("result", 'r') as result_json:
+			try:
+				result_dict = ast.literal_eval(result_json.read())
+				total_count  = sum(result_dict.values())
+				# Add actual data for making graph
+				for key in result_dict:
+					pronoun_graph.add(key, [(result_dict[key]/total_count)*100])
+			except Exception, e:
+				return(str(e))
+		# Giving the chart (pygal object) info
+		pronoun_graph.y_title = "The relative number of pronouns[%]"
+		pronoun_graph.title = "The number of pronouns"
+		pronoun_graph_data = pronoun_graph.render_data_uri()
+		return render_template("graphing.html", pronoun_graph_data = pronoun_graph_data)
+	else:
+        	return "No results found, run twitterCount\n"
+
 
 # Task that is beeing done by the celery workers
-@celery.task(name= 'celery_ex.tweetRetrieveAndCount')
-def tweetRetrieveAndCount():	
-	pronomen = {'han': 0, 'hon': 0 , 'hen': 0, 'den': 0, 'det': 0, 'denna': 0, 'denne': 0}
+@celery.task(name= 'celery_ex.tweetCount')
+def tweetCount():	
+	pronoun = {'han': 0, 'hon': 0 , 'hen': 0, 'den': 0, 'det': 0, 'denna': 0, 'denne': 0}
 	for data in conn.get_container(container_name)[1]:
 		obj_tuple = conn.get_object(container_name, data['name'])	
 		currentLine = 0
@@ -56,17 +82,18 @@ def tweetRetrieveAndCount():
 				tweet = json.loads(line)
 				if 'retweeted_status' not in tweet:
 					countsWord = Counter(tweet['text'].lower().split())
-					for key in pronomen:
+					for key in pronoun:
 						if key in countsWord:
-							pronomen[key] += countsWord[key]
+							pronoun[key] += countsWord[key]
 			except ValueError:
 				print(ValueError)
 			except:
 				continue
-	print(pronomen)
+	print("The total number of each pronoun is:")
+	print(pronoun)
 	with open("result", 'w') as result:
-		json.dump(pronomen, result)
-	return "End celery tweetRetrieve"
+		json.dump(pronoun, result)
+	return "Ending tweetCount"
 
 if __name__ == '__main__':
 	app.run(host = "0.0.0.0", debug=True)
